53fecc3079f9afded01e76759dc9bd0a86dc3043
Sharif Khan <sharifkhan74647464@gmail.com>
2024-08-22 20:59:15 +0600

    add more file upload functionality

--- utils.py
+++ utils.py
@@ -5,14 +5,39 @@ import pandas as pd
 
 def handle_csv_upload():
     global data
-    file_path = filedialog.askopenfilename(title="Select a file", filetypes=[("CSV files", "*.csv")])
+    file_path = filedialog.askopenfilename(
+        title="Select a file",
+        filetypes=[
+            ("CSV and Excel files", "*.csv;*.xlsx"),
+            ("CSV files", "*.csv"),
+            ("Excel files", "*.xlsx")
+        ]
+    )
+    
     if file_path:
-        df = pd.read_csv(file_path, dtype=str)
+        if file_path.lower().endswith('.csv'):
+            df = pd.read_csv(file_path, dtype=str)
+        elif file_path.lower().endswith('.xlsx'):
+            df = pd.read_excel(file_path, dtype=str)
         return df
 
 # Function to handle attachment upload
 def handle_attachment_upload():
-    file_types = [("PDF files", "*.pdf"), ("Image files", "*.jpg;*.jpeg;*.png;*.gif;*.bmp")]
+    file_types = [
+        ("All supported files", "*.pdf;*.csv;*.xlsx;*.jpg;*.jpeg;*.png;*.gif;*.bmp;*.txt;*.doc;*.docx;*.ppt;*.pptx;"),
+        ("PDF files", "*.pdf"),
+        ("CSV files", "*.csv"),
+        ("Excel files", "*.xlsx"),
+        ("JPEG files", "*.jpg;*.jpeg"),
+        ("PNG files", "*.png"),
+        ("GIF files", "*.gif"),
+        ("BMP files", "*.bmp"),
+        ("Text files", "*.txt"),
+        ("Word documents", "*.doc;*.docx"),
+        ("PowerPoint files", "*.ppt;*.pptx")
+    ]
+    
     file_paths = filedialog.askopenfilenames(title="Select files", filetypes=file_types)
+    
     if file_paths:
-        return file_paths 
+        return file_paths
